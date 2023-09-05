899fc6f49ca9afc5ecddeda799f830c55c3229d6
hamza-zaman001 <hamzabadiuzzamankhan@gmail.com>
2023-09-05 20:41:39 +0500

    updated

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -13,10 +13,13 @@ const FeatureCard = ({ icon, title, content, index }) => (
     </div>
     <div className='flex-1 flex flex-col ml-3'>
       <h4 className='font-poppins font-semibold
-      text-white text-[18px]'>
+      text-white text-[18px] leading-[23px]
+      mb-1'>
         {title}
       </h4>
-      <p>
+      <p className='font-poppins font-normal
+      text-dimWhite text-[16px] leading-[24px]
+      mb-1'>
         {content}
       </p>
     </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => {
   return (
     <button type='button' className={`py-4 px-6 bg-blue-gradient
     font-poppins font-medium text-[18px]
-    text-primary outline-none
+    text-primary outline-none rounded-md
     ${styles}`}>
       Get Started
     </button>
